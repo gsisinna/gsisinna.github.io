94c23def08e70e328c074d642c1b5f89783c6c54
Gabriele Sisinna <gabriele.sisinna@gmail.com>
2024-10-20 19:11:48 +0200

    Update packages, action and integrate gh pages workflow

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,5 @@ module.exports = defineConfig({
 })
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/gsisinna.github.io/'
-    : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
 };
